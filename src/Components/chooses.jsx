7d4297ec6fb1b7bd97bc350c0744ebfc18fb41c4
Nour Eldin <Nourraaft@gmail.com>
2023-01-20 14:57:39 +0200

    Create chooses.jsx

---
+++ src/Components/chooses.jsx
@@ -0,0 +1,77 @@
+
+import React from "react";
+import { BsFillDisplayFill } from "react-icons/bs";
+
+function Chooses() {
+   return (
+      <article className="choose" data-aos="fade-right">
+         <div className="container">
+            <h1>Why Choose us?</h1>
+            <main className="cards">
+               <section className="card" data-aos="fade-right">
+                  <div className="circle">
+                     <BsFillDisplayFill />
+                  </div>
+                  <h3>98% Success Rate</h3>
+                  <p>
+                     Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                     do amet sint. Velit officia consequatduis enim velit mollit
+                     Exer.
+                  </p>
+                  <button>Read More</button>
+               </section>
+               <section className="card" data-aos="fade-right">
+                  <div className="circle">
+                     <BsFillDisplayFill />
+                  </div>
+                  <h3>98% Success Rate</h3>
+                  <p>
+                     Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                     do amet sint. Velit officia consequatduis enim velit mollit
+                     Exer.
+                  </p>
+                  <button>Read More</button>
+               </section>
+               <section className="card" data-aos="fade-left">
+                  <div className="circle">
+                     <BsFillDisplayFill />
+                  </div>
+                  <h3>98% Success Rate</h3>
+                  <p>
+                     Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                     do amet sint. Velit officia consequatduis enim velit mollit
+                     Exer.
+                  </p>
+                  <button>Read More</button>
+               </section>
+               <section className="card" data-aos="fade-left">
+                  <div className="circle">
+                     <BsFillDisplayFill />
+                  </div>
+                  <h3>98% Success Rate</h3>
+                  <p>
+                     Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                     do amet sint. Velit officia consequatduis enim velit mollit
+                     Exer.
+                  </p>
+                  <button>Read More</button>
+               </section>
+               <section className="card" data-aos="fade-left">
+                  <div className="circle">
+                     <BsFillDisplayFill />
+                  </div>
+                  <h3>98% Success Rate</h3>
+                  <p>
+                     Amet minim mollit non deserunt ullamco est sit aliqua dolor
+                     do amet sint. Velit officia consequatduis enim velit mollit
+                     Exer.
+                  </p>
+                  <button>Read More</button>
+               </section>
+            </main>
+         </div>
+      </article>
+   );
+}
+
+export default Chooses;
