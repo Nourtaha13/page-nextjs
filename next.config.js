b8982a7e37c1a8582a12d4279e76e6dccb271c1e
Nour Eldin <Nourraaft@gmail.com>
2023-01-20 14:33:49 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,3 +6,23 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+module.exports = {
+   webpack(config, options) {
+      config.module.rules.push({
+         test: /\.(mp3)$/,
+         use: {
+            loader: "file-loader",
+            options: {
+               publicPath: "/_next/static/sounds/",
+               outputPath: "static/sounds/",
+               name: "[name].[ext]",
+               esModule: false,
+            },
+         },
+      });
+      return config;
+   },
+   experimental: {
+      appDir: true,
+   },
+};
