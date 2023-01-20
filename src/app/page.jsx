2d32d29e4cda1a64e9f8cbad426e1ac54af96347
unknown <nourraaft@gmail.com>
2023-01-20 14:25:16 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,14 @@
 "use client"
 import React, { useEffect } from 'react'
-import Navbar from '@/Components/Navbar'
-import Hero from '@/Components/Hero'
-import Ourself from '@/Components/Ourself'
-import Choose from '@/Components/choose'
-import Practices from '@/Components/Practices'
-import OurTeam from '@/Components/OurTeam'
-import Faq from '@/Components/Faq'
-import Subscribe from '@/Components/Subscribe'
-import Footer from '@/Components/footer'
+import Navbar from '../Components/Navbar'
+import Hero from '../Components/Hero'
+import Ourself from '../Components/Ourself'
+import Choose from '../Components/choose'
+import Practices from '../Components/Practices'
+import OurTeam from '../Components/OurTeam'
+import Faq from '../Components/Faq'
+import Subscribe from '../Components/Subscribe'
+import Footer from '../Components/footer'
 import AOS from "aos"
 import "aos/dist/aos.css"
 
